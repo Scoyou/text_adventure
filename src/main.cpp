d777efe69980ea5f33d9658bb1d40b8c248e858f
Scott Young <youngscottj@gmail.com>
2025-08-29 19:46:52 -0600

    refactor main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,63 @@
 #include <iostream>
 #include "player/PlayerCharacterUtils.h"
 #include "dungeon/DungeonUtils.h"
+#include "monster/MonsterUtils.h"
 
 int clearBuffer()
 {
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     return 0;
+}
+
+void roomActions(Room &current_room)
+{
+    if (current_room.has_monster)
+    {
+        std::cout << "You see a monster!" << std::endl;
+        Monster monster = createMonster("Goblin", "A small goblin", "Goblin", 10, 5, 2);
+
+        std::cout << "Name: " << monster.getName() << std::endl;
+        std::cout << "Description: " << monster.getDescription() << std::endl;
+        std::cout << "Race: " << monster.getRace() << std::endl;
+        std::cout << "Health: " << monster.getHealth() << std::endl;
+        std::cout << "Attack: " << monster.getAttack() << std::endl;
+        std::cout << "Defense: " << monster.getDefense() << std::endl;
+    }
+    else if (current_room.has_treasure)
+    {
+        std::cout << "You see a treasure!" << std::endl;
+    }
+    else if (current_room.has_trap)
+    {
+        std::cout << "You see a trap!" << std::endl;
+    }
+}
+
+void roomNavigation(Room &current_room, Dungeon &dungeon)
+{
+    std::cout << "You can go: ";
+    for (const auto &kv : current_room.connected_rooms)
+    {
+        std::cout << kv.first << ", ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Enter direction (N, S, E, W): ";
+    std::string input;
+    std::cin >> input;
+
+    // Transform input to uppercase
+    std::transform(input.begin(), input.end(), input.begin(), ::toupper);
+
+    if (current_room.connected_rooms.find(input) != current_room.connected_rooms.end())
+    {
+        int next_room_id = current_room.connected_rooms[input];
+        current_room = dungeon.getRoom(next_room_id); // move to that room
+    }
+    else
+    {
+        std::cout << "You can't go that way!" << std::endl;
+    }
 }
 
 int main()
@@ -65,29 +117,9 @@ int main()
                   << ", Treasure: " << current_room.has_treasure
                   << ", Trap: " << current_room.has_trap << std::endl;
 
-        std::cout << "You can go: ";
-        for (const auto &kv : current_room.connected_rooms)
-        {
-            std::cout << kv.first << ", ";
-        }
-        std::cout << std::endl;
+        roomActions(current_room);
 
-        std::cout << "Enter direction (N, S, E, W): ";
-        std::string input;
-        std::cin >> input;
-
-        // Transform input to uppercase
-        std::transform(input.begin(), input.end(), input.begin(), ::toupper);
-
-        if (current_room.connected_rooms.find(input) != current_room.connected_rooms.end())
-        {
-            int next_room_id = current_room.connected_rooms[input];
-            current_room = dungeon.getRoom(next_room_id); // move to that room
-        }
-        else
-        {
-            std::cout << "You can't go that way!" << std::endl;
-        }
+        roomNavigation(current_room, dungeon);
     }
 
     std::cout << "Congratulations! You reached the exit room!" << std::endl;
